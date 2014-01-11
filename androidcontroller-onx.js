7ea5e5374d4c13e7953bcfe56ac1ba5c53dd76ba
Patrick Catanzariti <thatpatrickguy@gmail.com>
2014-01-11 18:39:25 +1100

    Added the loop to check inputs

---
+++ androidcontroller-onx.js
@@ -0,0 +1,54 @@
+device.telephony.on('incomingCall', function (signal) 
+{
+	device.notifications.createNotification("INCOMING! " + signal.phoneNumber).show();
+
+	console.info(signal);
+
+	device.scheduler.setTimer({
+		name: "checkingForInCallInputs", 
+		time: 0,
+		interval: 5*1000,
+		exact: false
+	},
+		function () {
+			checkIfPhoneShouldBeSilent();
+		}
+	);
+});
+
+device.telephony.on('idle', function () {
+	device.notifications.createNotification("No longer in a call, I'll stop asking.").show();
+
+	device.scheduler.removeTimer("checkingForInCallInputs");
+
+	returnToPhoneDefaults();
+});
+
+function checkIfPhoneShouldBeSilent() {
+	device.notifications.createNotification('Asking if I should be silent...').show();
+
+	device.ajax({
+		url: 'http://androidcontroller.herokuapp.com/shouldibesilent',
+		type: 'POST',
+		dataType: 'json',
+		data: '{"call":"incoming"}',
+		headers: {'Content-Type':'application/json'}
+	}, function onSuccess(body, textStatus, response) {
+		console.info('successfully received http response!');
+		console.info(response);
+
+		if (response.callSound == false) {
+			device.audio.ringerVolume = 0;
+		}
+
+	}, function onError(textStatus, response) {
+		var error = {};
+		error.message = textStatus;
+		error.statusCode = response.status;
+		console.error('error: ',error);
+	});
+}
+
+function returnToPhoneDefaults() {
+	device.audio.ringerVolume = 80;
+}
